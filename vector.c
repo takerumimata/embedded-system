769ea313410b71ac917ec2c2b3f995089de5a2ee
takerumimata <fossil1908@gmail.com>
2020-09-17 13:36:54 +0900

    define Interrupt vector

---
+++ vector.c
@@ -0,0 +1,19 @@
+#include "defines.h"
+
+extern void start(void);    // スタートあっぷ
+
+/*
+    割り込みベクタの設定
+    リンカ・スクリプトの定義により、銭湯番地に配置される。
+*/
+
+void (*vectors[])(void) {
+    start, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+}；
