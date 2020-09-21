2513bd7ed991df6874ef56436e7303f08470d0c5
takerumimata <fossil1908@gmail.com>
2020-09-21 23:48:05 +0900

    add comments

--- src/01/bootload/defines.h
+++ src/01/bootload/defines.h
@@ -1,9 +1,10 @@
 #ifndef _DEFINES_H_INCLUDED_
 #define _DEFINES_H_INCLUDED_
 
-#define NULL ((void *)0)
-#define SERIAL_DEFAULT_DEVICE 1
+#define NULL ((void *)0) // NULLポインタの定義
+#define SERIAL_DEFAULT_DEVICE 1 // 標準のシリアル・デバイス
 
+// ビット幅固定の整数型
 typedef unsigned char uint8;
 typedef unsigned char uint16;
 typedef unsigned char uint32;
