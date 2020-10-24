45b6ba43019e232f01170818248a97b5b4e0fcca
takerumimata <fossil1908@gmail.com>
2020-10-24 14:49:55 +0900

    copy from chapter1

---
+++ src/02/bootload/main.c
@@ -0,0 +1,14 @@
+#include "defines.h"
+#include "serial.h"
+#include "lib.h"
+
+int main(void) {
+    serial_init(SERIAL_DEFAULT_DEVICE); // シリアルデバイスを初期化
+
+    puts("Hello World!\n"); // Hello World!を出力
+
+    while (1)   // 無限ループで停止
+        ;
+    
+    return 0;
+}
